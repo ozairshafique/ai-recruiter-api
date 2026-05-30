979260e98c223abefdb9c0e6d1e56cd8c5562317
Ozair Shafique <bc090402541@gmail.com>
2026-05-31 01:25:20 +0200

    fix import statement for RecursiveCharacterTextSplitter in ingestion.py

--- services/ingestion.py
+++ services/ingestion.py
@@ -5,7 +5,7 @@ import time
 from core.config import get_settings
 from core.logging import get_logger
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 logger = get_logger(__name__)
