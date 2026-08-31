94e87dfa7d39f1b7d08cdc948e23bd70c2d4b446
Uzair Shafique <bc090402541@gmail.com>
2026-08-31 21:42:10 +0200

    feat: update CORS middleware to restrict allowed origins and methods - deploy

--- app/main.py
+++ app/main.py
@@ -85,8 +85,12 @@ debug=settings.debug)
 # ── Middleware ──────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
+    allow_origins=[
+        "https://ai-recruiter-frontend-khaki.vercel.app",
+        "http://localhost:8000",
+        "http://localhost:3000",
+    ],
+    allow_methods=["GET", "POST", "DELETE"],
     allow_headers=["*"],
     allow_credentials=True,
 
